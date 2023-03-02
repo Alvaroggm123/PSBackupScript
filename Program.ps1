a895319a6ce79c9c3bc8031d68d79962746920c5
Alvaro González <aggm123@icloud.com>
2023-03-02 13:59:22 -0800

    Comentarios a lib menu.

--- Program.ps1
+++ Program.ps1
@@ -1,24 +1,34 @@
+# +------------------------------------------------------+
+# +             Referencia a bibliotecas                 +
+# +------------------------------------------------------+
 . .\lib\Menu.ps1
 # +------------------------------------------------------+
 # +                Comienza ejecución                    +
 # +------------------------------------------------------+
 
+# +------------------------+
+# +    Creación de Menú    +
+# +------------------------+
 [array]$opciones = "Realizar respaldo general", "Respaldar último respaldo", "Eliminar respaldo más viejo"
 $menuMain = [classMenu]::new("Menú principal", $opciones)
-$respuesta = $menuMain.start(1)
 
-switch ($respuesta) {
+# +------------------------+
+# +  Opciones del Menú     +
+# +------------------------+
+switch ($menuMain.start(1)) {
     1 { 
         Write-Host "Opcion 1 ejecutada correctamente"
-     }
+        Pause
+    }
     2 { 
         Write-Host "Opcion 2 ejecutada correctamente"
+        Pause
      }
     Default {}
 }
 
-# +---------------------+
-# + LOG de la ejecución +
-# +---------------------+
+# +------------------------------------------------------+
+# +                Log de la ejecución                   +
+# +------------------------------------------------------+
 Start-Transcript -Append .\log\log_Program.txt
 Clear-Host
--- lib/menu.ps1
+++ lib/menu.ps1
@@ -1,16 +1,31 @@
-# +--------------------+
-# + Clase Menú         +
-# +--------------------+
+# +------------------------------------------------------+
+# +                     Clase Menú                       +
+# +------------------------------------------------------+
+# + Se trata de un script que permite generar instancias +
+# + de la clase classMenu, estas permiten asociar arrays +
+# + con las respectivas opciones a escoger, además,      +
+# + cuenta con la posibilidad de enviar una respuesta    +
+# + que funcióne como la opción "Por defecto".           +
+# +------------------------------------------------------+
 class classMenu {
+    # +----------------------------------+
+    # + Atributos de la clase            +
+    # +----------------------------------+
     [string]$menuName
     [array]$menuOptions
-    [int]$timer = 60
+    [int]$menuTimer = 60
 
+    # +----------------------------------+
+    # + Constructores de la clase        +
+    # +----------------------------------+
+    
     # +---------------------------+
-    # + Constructores de la clase +
+    # + Constructor principal     +
     # +---------------------------+
-
-    # Constructor para generar instancia de la clase solo con el nombre.
+    # + Este constructor solo     +
+    # + requiere del nombre de la +
+    # + instancia o "Título".     +
+    # +---------------------------+
     classMenu([string]$menuName) {
         [array]$EndOption = "Exit"
 
@@ -18,7 +33,15 @@ class classMenu {
         $this.menuOptions = "No options", "available"
         $this.menuOptions += $EndOption
     }
-    # Sobrecarga de constructor para inicializar con las opciones.
+    
+    # +---------------------------+
+    # + Constructor sobrecargado  +
+    # +---------------------------+
+    # + Este constructor requiere +
+    # + del nombre de la instancia+
+    # + y el conjunto de opciones +
+    # + que tendrán.              +
+    # +---------------------------+
     classMenu([string]$menuName, [array]$menuOptions) {
         [array]$EndOption = "Exit"
         $this.menuName = $menuName
@@ -26,38 +49,79 @@ class classMenu {
         $this.menuOptions += $EndOption
     }
 
-    # Sobrecarga de constructor para inicializar con las opciones y timer.
-    classMenu([string]$menuName, [array]$menuOptions, [int]$timer) {
+    # +---------------------------+
+    # + Constructor con tiempo    +
+    # +---------------------------+
+    # + Este constructor requiere +
+    # + de todos los parametros   +
+    # + incluyendo el tiempo      +
+    # + límite para responder.    +
+    # +---------------------------+
+    classMenu([string]$menuName, [array]$menuOptions, [int]$menuTimer) {
         [array]$EndOption = "Exit"
         $this.menuName = $menuName
         $this.menuOptions = $menuOptions
         $this.menuOptions += $EndOption
-        $this.timer = $timer
+        $this.timer = $menuTimer
     }
     
+    # +----------------------------------+
+    # + funciónes y métodos de la clase  +
+    # +----------------------------------+
+    
+    # +---------------------------+
+    # + función oculta "Choose"   +
+    # +---------------------------+
+    # + función encargada de      +
+    # + enlistar las opciones     +
+    # + disponibles.              +
+    # +---------------------------+
     hidden [int] Choose([int]$default) {
-        # Impresión de "Opcion (<LaDeDefecto>): "
+
+        # +-----------------------+
+        # + Impresíón de espera   +
+        # + de respuesta.         +
+        # +-----------------------+
         Write-Host "Choose (" -NoNewline -ForegroundColor Cyan
         Write-Host "default" -NoNewline -ForegroundColor red
         Write-Host " = " -NoNewline -ForegroundColor Cyan
         Write-Host $default -NoNewline -ForegroundColor Green
         Write-Host "): " -NoNewline -ForegroundColor Cyan
-    
-        # Obtenemos respuesta del usuario
+
+        # +-----------------------+
+        # + Obtenemos respuesta   +
+        # + del usuario.          +
+        # +-----------------------+
         $answ = $this.$default
         try {
             $answ = Read-Host
             return $answ
         }
         catch {
+            
+            # +-------------------+
+            # + En caso de encon- +
+            # + rar un error, se  +
+            # + generará un log.  +
+            # +-------------------+
             Write-Host "Error with input, must type a number..."
             Start-Transcript -Append .\log\log_menu.txt
         }
     
-        # Retornamos respuesta
+        # +-----------------------+
+        # + Retorno de respuesta. +
+        # +-----------------------+
         return $answ
     }
     
+    # +---------------------------+
+    # + función oculta "funcMenu" +
+    # +---------------------------+
+    # + función encargada de      +
+    # + imprimir el título del    +
+    # + menú instanciado y las    +
+    # + opciones asociadas.       +
+    # +---------------------------+
     hidden [int] funcMenu ([int]$default) {
         Clear-Host
         [int]$counter = 0
@@ -69,29 +133,83 @@ class classMenu {
         return $this.Choose($default)
     }
     
+    # +---------------------------+
+    # + función oculta "start"    +
+    # +---------------------------+
+    # + Esta función es la que se +
+    # + encarga de regresar el    +
+    # + valor/"opción" que el     +
+    # + usuario escoja, esto      +
+    # + considerando a la última  +
+    # + opción como la opción de  +
+    # + salida del programa.      +
+    # + Se debe considerar que la +
+    # + opción por defecto es la  +
+    # + salida del programa.      +
+    # +---------------------------+
     [int] start (){
         do {
+            # +-------------------+
+            # + Captura de Choose +
+            # +-------------------+
             [int]$choosenIndex = $this.funcMenu($this.menuOptions.Length)
+            
+            # +-------------------+
+            # + Se valida si el   +
+            # + usuario está      +
+            # + activo en la      +
+            # + terminal.         +
+            # +-------------------+
             if ($choosenIndex -lt 1 ){
                 return $this.menuOptions.Length
             }
         } while (
-            
+            # +-------------------+
+            # + Validación de     +
+            # + Salida del prog,  +
+            # +-------------------+
             $choosenIndex -gt $this.menuOptions.Length -or !$choosenIndex
         )
+        # +-----------------------+
+        # + Retorno de opción     +
+        # +-----------------------+
         return $choosenIndex
     }
     
+    # +---------------------------+
+    # + Sobrecarga de la función  +
+    # + start                     +
+    # +---------------------------+
+    # + Esta función permite      +
+    # + asociar una opción por    +
+    # + defecto.                  +
+    # +---------------------------+
     [int] start ([int]$default){
         do {
+            # +-------------------+
+            # + Captura de Choose +
+            # +-------------------+
             [int]$choosenIndex = $this.funcMenu($default)
+
+            # +-------------------+
+            # + Se valida si el   +
+            # + usuario escogió   +
+            # + la opción por     +
+            # + defecto.          +
+            # +-------------------+
             if ($choosenIndex -lt 1 ){
                 return $default
             }
         } while (
-            
+            # +-------------------+
+            # + Validación de     +
+            # + Salida del prog,  +
+            # +-------------------+
             $choosenIndex -gt $this.menuOptions.Length -or !$choosenIndex
         )
+        # +-----------------------+
+        # + Retorno de opción     +
+        # +-----------------------+
         return $choosenIndex
     }
 }
