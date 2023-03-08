fb245f226e20494dcee8c4160f956420ac888fd9
Alvaro González <aggm123@icloud.com>
2023-03-08 07:59:29 -0800

    Claridad en la consola.

--- lib/menu-gpt.ps1
+++ lib/menu-gpt.ps1
@@ -10,19 +10,24 @@ class Menu {
     [void]Show() {
         Clear-Host
         $consoleWidth = [Console]::WindowWidth - 2
-        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth-2)))
-        Write-Host ("|" + " [{0,-7}] | {1,-29}" -f "Option", "Description") -NoNewline
+        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth - 2)))
+        Write-Host ("|" + " [{0,-7}] | {1,-29}" -f " Option", "Description") -NoNewline
         [Console]::SetCursorPosition(([Console]::WindowWidth - 1), 1)
         Write-Host ("|")
-        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth-2)))
+        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth - 2)))
         foreach ($item in $this.Options.GetEnumerator()) {
             $optionText = "[ {0,5} ] | {1,-25}" -f $item.Key, $item.Value
             $optionWidth = $optionText.Length
             $paddingWidth = $consoleWidth - $optionWidth - 3
             Write-Host ("|" + " {0} {1,-$paddingWidth} |" -f $optionText, (" " * ($paddingWidth - 1)))
         }
-        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth-2)))
-        Write-Host ("Opción: ") -NoNewline
+        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth - 2)))
+        $oldTop = [Console]::CursorTop
+        Write-Host ("|" + " {0,-$($consoleWidth-2)} |" -f (" " * ($consoleWidth - 2)))
+        Write-Host ("+" + " {0,-$($consoleWidth-2)} +" -f ("-" * ($consoleWidth - 2)))
+        [Console]::SetCursorPosition(5, $oldTop)
+        Write-Host ("[Press <ENTER> to choose an option] | Option: ") -NoNewline
+        [Console]::SetCursorPosition([Console]::CursorLeft, $oldTop)
     }
         
     
@@ -34,7 +39,7 @@ class Menu {
         $timer.Start()
         while ($timer.Elapsed.TotalSeconds -lt $timeoutSeconds) {
             $timeLeft = $timeoutSeconds - [math]::Round($timer.Elapsed.TotalSeconds)
-            $timerString = "Time left: {0:mm\:ss}" -f ([datetime]"00:00:00").AddSeconds($timeLeft)
+            $timerString = "| Time left: {0:mm\:ss}" -f ([datetime]"00:00:00").AddSeconds($timeLeft)
             [Console]::SetCursorPosition(([Console]::WindowWidth - $timerString.Length - 3), 1)
             [Console]::Write($timerString)
 
@@ -54,7 +59,7 @@ class Menu {
     
         [Console]::SetCursorPosition(0, [Console]::CursorTop)
         [Console]::Write((" " * [Console]::WindowWidth).PadRight([Console]::WindowWidth))
-        [Console]::SetCursorPosition(0, [Console]::CursorTop+1)
+        [Console]::SetCursorPosition(0, [Console]::CursorTop + 1)
     
         Write-Host "No option selected. Default option ($($this.Options[$this.DefaultOption])) selected."
         return $this.DefaultOption
